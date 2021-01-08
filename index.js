d06199e474754b9e5db72d23dd9910cb5fa1cda7
giamdoc <giamdoc.gate@github.com>
2021-01-08 17:18:27 +0700

    Ngay 8 thang 1

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const logger = require('./middleware/logger');
+
+// Init middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname)));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
---
+++ routes/api/members.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const uuid = require('uuid');
+const router = express.Router();
+const members = require('../../members');
+
+// Gets All members
+router.get('/', (req, res) => res.json(members));
+
+// Get Single Member
+router.get('/:id', (req, res) => {
+    // res.send(req.params.id);
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+    }
+});
+
+// Creater Member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+    console.log(newMember);
+
+    if(!newMember.name || !newMember.email) {
+        res.status(400).json({ msg: 'Please include  a name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+
+    
+});
+
+
+
+module.exports = router;
